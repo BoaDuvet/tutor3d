04e99fa55078f9dae9c65d2d93e6609a7b6816b2
BoaDuvet <margoshnya7@gmail.com>
2025-10-02 17:29:58 +0500

    cam

--- player.gd
+++ player.gd
@@ -13,3 +13,16 @@ func _physics_process(delta):
 		direction.z += 1
 	if Input.is_action_pressed("move_forward"):
 		direction.z -= 1
+		
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+		$Pivot.basis = Basis.looking_at(direction)
+	
+	
+	target_velocity.x = direction.x * speed
+	target_velocity.z = direction.z * speed
+
+	if not is_on_floor(): 
+		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+	velocity = target_velocity
+	move_and_slide()
